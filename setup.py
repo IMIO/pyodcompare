e13c14dbb19735a42660a08c50338a4a6c21df13
fngaha <franck.o.ngaha@gmail.com>
2015-06-04 15:59:46 +0200

    Prepare release

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ setup(name='pyodcompare',
       description="Generate a comparaison of two libreoffice documents using uno",
       long_description="""\
 Use libreoffice compare feature to generate an openoffice document with modifications registered from two versions of a document""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7"]
+      , # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='libreoffice openoffice',
       author='Thomas Desvenain',
       author_email='thomas.desvenain@gmail.com',
-      url='',
+      url='https://github.com/IMIO/pyodcompare',
       license='GPL v3',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
